c24592185170697af9a9a7056f79b2d9376d49c5
EissaAlzabidi <>
2021-09-30 10:55:33 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+# Input Data بدون التعديل علي الـ
+# Output لكل سؤال اطبع الـ
+##########################
+# Input Data:
+numbers = "101, 102,103,    104, 105"
+names = ["sabah", "Sami", "salim", "mohammed", "Tim"]
+
+###########################
+# Q1: ترنيب الأسماء حسب الحروف تنازليا " descending order" 
+# Output:
+# ['Tim', 'Sami', 'salim', 'sabah', 'mohammed']
+
+###########################
+# Q2: ترنيب الأسماء حسب عدد الحروف من الكبير الى الصغير
+# Output:
+# ['mohammed', 'sabah', 'salim', 'Sami', 'Tim']
+
+###########################
+# Q3: ترنيب الأسماء حسب الحروف الثاني من تصاعديا "ascending order"
+# Output:
+# ['sabah', 'salim', 'Sami', 'Tim', 'mohammed']
+
+###########################
+#  Q4: numbers دمح رقم كل طالب من متغير  
+#      names مع ما يقابله من من  
+# Output:
+# [('1', 'sabah'), ('2', 'Sami'), ('3', 'salim'), ('4', 'mohammed'), ('5', 'Tim')]
